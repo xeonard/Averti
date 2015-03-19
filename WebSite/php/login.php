c609bbac7a3959eab8daa701ea5ed2661fc12b33
dbunin <bunind@live.com>
2015-03-19 12:34:39 +0100

    WebSite-code
    Homepage and Learn More page by Bunin Dmytro

---
+++ WebSite/php/login.php
@@ -0,0 +1,12 @@
+<?php?> 
+<div id="logindiv">
+        <form class="form" action="#" id="login">
+            <h3>Login Form</h3>
+            <label>Username :</label>
+            <input type="text" id="username" placeholder="Enter your username"/>
+            <label>Password : </label>
+            <input type="text" id="password" placeholder="Enter your password"/>
+            <input type="button" id="loginbtn" value="Login"/>
+            <input type="button" id="cancel" value="Cancel"/>
+        </form>
+    </div>
