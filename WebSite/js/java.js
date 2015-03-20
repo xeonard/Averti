2981935a71256f89763b8cb94bd62ce350a2adb5
dbunin <bunind@live.com>
2015-03-20 15:06:44 +0100

    Added login form
    Added popup login form and added jQuery for closing. By Bunin Dmytro.

--- WebSite/js/java.js
+++ WebSite/js/java.js
@@ -1,9 +1,11 @@
+
 $(document).ready(function(){
-  $('#login-trigger').click(function(){
-    $(this).next('#login-content').slideToggle();
-    $(this).toggleClass('active');          
-    
-    if ($(this).hasClass('active')) $(this).find('span').html('&#x25B2;')
-      else $(this).find('span').html('&#x25BC;')
-    })
+  $('#login').click(function(){
+	$('.login').css("visibility", "visible");
+  })
+  
+  $('#close').click(function(){
+	$('.login').css("visibility", "hidden");
+  })
 });
+
