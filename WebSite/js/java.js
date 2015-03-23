3b3ecd423968e3a38ab9f9ff06eedda1c8ea8ce7
dbunin <bunind@live.com>
2015-03-23 15:07:51 +0100

    Added sign in for and start working on profile page
    By Dmytro Bunin

--- WebSite/js/java.js
+++ WebSite/js/java.js
@@ -1,14 +1,41 @@
 
 $(document).ready(function(){
+	//Hides forms
 	$('.login').hide();
+	$('.signup').hide();
+	
 	//Method for showing the log in form
   $('#login').click(function(){
 	$('.login').show("slow");
+	$('.signup').hide();
   });
   
   	//method for closing the log in form
-  $('.btnClose').click(function(){
-	$('.login').hide("slow");
+  $('.btnCloseLogin').click(function(){
+	$('.login').hide();
+  });
+	
+	//Method for showing the sign up form
+  $('#signup').click(function(){
+	$('.signup').show("slow");
+	$('.login').hide();
+  });
+  
+  	//method for closing the sign up form
+  $('.btnCloseSignUp').click(function(){
+	$('.signup').hide("slow");
+  });
+	
+	//show map on profile page
+  $('.btnmap').click(function(){
+	 $('.map').show(); 
+	 $('.profinfo').hide();
+  });
+	
+	//show profile info on profile page
+	$('.btnprofinfo').click(function(){
+	 $('.map').hide(); 
+	 $('.profinfo').show();
   });
 });
 
