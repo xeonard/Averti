4627a9b939aeb4a0e2223805d87e2c0036c59d7b
Francisco Vrolijk <xeonard@hotmail.com>
2015-05-05 15:32:28 +0200

    Connection to DB in DBhandler

--- Averti_Festival_application/Averti_Festival_application/DBHandler.cs
+++ Averti_Festival_application/Averti_Festival_application/DBHandler.cs
@@ -3,10 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace AvertiFestivalApplication
 {
     class DBHandler
     {
+        private MySqlConnection connection;
+
+        public DBHandler()
+        {
+            String connectInfo =    "server=athena01.fhict.local;" +
+                                    "database=dbi252284;" +
+                                    "user id=dbi252284;" +
+                                    "password=fCPIXLUNGi;" +
+                                    "connect timeout=30;";
+
+            connection = new MySqlConnection(connectInfo);
+        }
+
+        public String GetAllNames()
+        {
+            //another test method
+            String sql = "SELECT name FROM Person";
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            String names = String.Empty;
+
+            try
+            {
+                connection.Open();
+
+                MySqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    names += reader[1] + "/n";
+                }
+            }
+            catch
+            {
+                throw new Exception("Error with DB");
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return names;
+        }
+
+        public Boolean CheckRfid(String rfid)
+        {
+            //test method?
+
+            return true;
+        }
     }
 }
